8437b08917b662e029c2f89d3abf6a1e6207c20f
payssion <github@payssion.com>
2019-03-27 10:59:51 +0800

    remove upload folder

--- upload/catalog/controller/extension/payment/payssionepsat.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once(realpath(dirname(__FILE__)) . "/payssion.php");
-class ControllerExtensionPaymentPayssionEpsat extends ControllerExtensionPaymentPayssion {
-	protected $pm_id = 'eps_at';
-}
--- upload/catalog/model/extension/payment/payssionepsat.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once(realpath(dirname(__FILE__)) . "/payssion.php");
-class ModelExtensionPaymentPayssionEpsat extends ModelExtensionPaymentPayssion {
-}
